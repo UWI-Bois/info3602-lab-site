11708149b447f49efebaeb164b9916e2cb539246
Ajay <ajay_sieunarine@hotmail.com>
2020-04-28 14:39:53 -0400

    search html and js with timer

--- wp-content/themes/my-website-theme/js/modules/Search.js
+++ wp-content/themes/my-website-theme/js/modules/Search.js
@@ -7,29 +7,52 @@ class Search{
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
+
         this.events();
+        this.isOverlayOpen = false;
+        this.typingTimer;
+        this.resultsDiv = $("#search-overlay__results");
     }
 
     //events
     events(){
-        this.openButton.on("click", this.openOverlay.bind(this));
+        this.openButton.on("click", this.openOverlay.bind(this)); // .bind ensures that the function stays within this object's context, instead of making its own.
         this.closeButton.on("click", this.closeOverlay.bind(this));
+        this.searchField.on("keydown", this.typingLogic.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    }
+    // typing logic
+    typingLogic(){
+        // alert('typing logic says hi :)'); // working
+        // setTimeout(function () {
+        //     console.log("timeout test")
+        // }, 2000);
+        clearTimeout(this.typingTimer);
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+    }
+    getResults(){
+        // send to wordpress server
+        console.log("RESULTS");
+        this.resultsDiv.html("Imagine search results here...");
     }
     // open overlay
     openOverlay(){
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll"); // prevent scrolling while searching
+        this.isOverlayOpen = true;
     }
     // close overlay
     closeOverlay(){
         this.searchOverlay.removeClass("search-overlay--active");
-        $("body").removeClass("body-no-scroll"); // restore scrolling ability when the search is closed
+        $("body").removeClass("body-no-scroll"); // restore scrolling ability when the search is closed3
+        this.isOverlayOpen = false;
     }
     // key event methods
     keyPressDispatcher(e){
         console.log("this is a test")
-        if(e.keyCode == 83) this.openOverlay();
+        if(e.keyCode == 83 && !this.isOverlayOpen) this.openOverlay(); // open the search overlay if 's' is pressed
+        if(e.keyCode == 27 && this.isOverlayOpen) this.closeOverlay(); // close the search overlay if 'esc' is pressed
     }
 }
 export default Search;
