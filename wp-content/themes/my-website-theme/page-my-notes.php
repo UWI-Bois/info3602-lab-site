a70b7baad25a8c0f7fc9023c97dbacd98b544f8e
Ajay <ajay_sieunarine@hotmail.com>
2020-04-30 18:55:01 -0400

    added my notes, buttons need functionality

--- wp-content/themes/my-website-theme/page-my-notes.php
+++ wp-content/themes/my-website-theme/page-my-notes.php
@@ -12,6 +12,43 @@ while (have_posts()) { // iterate through all posts
 
     <div class="container container--narrow page-section">
 
+        <ul class="min-list link-list" id="my-notes">
+<!--            query the wordpress db for relevant posts-->
+            <?php
+            $userNotes = new WP_Query(
+                    array(
+	                    'post_type' => 'note',
+	                    'posts_per_page' => -1,
+	                    'author' => get_current_user_id() // get notes for signed in user
+                    )
+            );
+
+            while($userNotes->have_posts()){
+                $userNotes->the_post();
+            ?>
+                <li>
+                    <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+
+<!--                    crud buttons-->
+                    <span class="edit-note">
+                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                        Edit
+                    </span>
+                    <span class="delete-note">
+                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        Delete
+                    </span>
+
+                    <textarea class="note-body-field"><?php echo esc_attr(strip_tags(get_the_content())); ?></textarea>
+                </li>
+
+                <?php
+                wp_reset_postdata();
+            }// end while
+                ?>
+
+        </ul>
+
 
     </div>
 
